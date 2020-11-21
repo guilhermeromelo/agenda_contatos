f65d0682a32a01d1e466ef176de4c390839ff354
Guilherme Melo <guilhermeromelo@gmail.com>
2020-11-21 03:10:23 -0300

    Finalização do CRUD e funções do banco de dados
    Criei O CRUD, e algumas funções para manipular o banco de dados.

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,51 +1,93 @@
 import 'package:sqflite/sqflite.dart';
-
-final String idColumn = "idColumn";
-final String nameColumn = "nameColumn";
-final String emailColumn = "emailColumn";
-final String phoneColumn = "phoneColumn";
-final String imgColumn = "imgColumn";
-
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'package:agenda_contatos/helpers/Contact.dart';
 
 class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+  factory ContactHelper() => _instance;
+  ContactHelper.internal();
 
-}
-// ID  Name  Email  phone img
-// 1   Gui   @gui  3333   local
-class Contact {
-  int id;
-  String name;
-  String email;
-  String phone;
-  String img;
+  //CRIA A VARIAVEL DO TIPO BANCO DE DADOS
+  Database _db;
 
-  Contact.fromMap(Map map){
-    id = map[idColumn];
-    name = map[nameColumn];
-    email = map[emailColumn];
-    phone = map[phoneColumn];
-    img = map[imgColumn];
-  }
-
-  Map toMap(){
-    Map<String, dynamic> map = {
-      nameColumn: name,
-      emailColumn: email,
-      phoneColumn: phone,
-      imgColumn: img,
-    };
-    if(id!=null){
-      map[idColumn] = id;
+  //CRIAR OU RECUPERAR O BANCO DE DADOS - REPASSA O BANCO PARA TODAS AS FUNÇÕES
+  Future<Database> get db async{
+    if(_db != null){
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
     }
-    return map;
-
-
-
   }
 
-  @override
-  String toString() {
-    return 'Contact{id: $id, name: $name, email: $email, phone: $phone, img: $img}';
+  //CRIAR O BANCO DE DADOS
+  Future<Database> initDb() async{
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    openDatabase(path,version: 1, onCreate: (Database db, int newerVersion)async{
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, "
+            "$phoneColumn TEXT, $imgColumn TEXT)"
+      );
+    });
   }
-  
+
+  //CRUD - CREATE
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  //CRUD - READ
+  Future<Contact> getContact(int id) async{
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+    columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn], where: "$idColumn = ?", whereArgs: [id]);
+    if(maps.length>0){
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  //CRUD - UPDATE
+  Future<int> updateContact(Contact contact) async{
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?", whereArgs: [contact.id]);
+  }
+
+  //CRUD - DELETE
+  Future<int> deleteContact(int id) async{
+    Database dbContact = await db;
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  //CARREGAR TODOS OS CONTATOS - MODIFIQUEI POR MINHA CONTA E RISCO
+  Future<List> getAllContacts() async{
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContact = List();
+    for(int i; i<listMap.length;i++){
+      Map m = listMap.elementAt(i);
+      listContact.add(Contact.fromMap(m));
+    }
+    return await listContact;
+  }
+
+  //RETORNA A QUANTIDADE DE CONTATOS
+  Future<int> getNumber() async{
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+  //FECHA O BANCO DE DADOS
+  close()async{
+    Database dbContact = await db;
+    dbContact.close();
+  }
+
 }
+
